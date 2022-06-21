cdff912b09735db3d8ed136b4040297e14c840f0
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-21 18:40:27 -0400

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,1 @@
-from db import get, select, search, exist, add, create, edit, replace, remove, delete, searchcat, asearchcat, createcat, acreatecat, deletecat, adeletecat, listcat, alistcat, prefix, aprefix, clearprefix, cleanprefix, restore, arestore, clearcache, cleancache, transferjson, atransferjson
+from .db import get, select, search, exist, add, create, edit, replace, remove, delete, searchcat, asearchcat, createcat, acreatecat, deletecat, adeletecat, listcat, alistcat, prefix, aprefix, clearprefix, cleanprefix, restore, arestore, clearcache, cleancache, transferjson, atransferjson
